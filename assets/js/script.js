fd1dbe445b965ff8ce59e69b541a53c35f54e087
Colin <colinkeany@github.com>
2021-07-05 11:23:43 -0400

    remove unused images, update styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,33 @@ function readURL(input) {
 		reader.readAsDataURL(input.files[0]);
 	}
 }
+
+$('document').ready(function() {
+	$('.custom').click(function() {
+		var customURL = $('.customURL').val();
+		$('.favicon')
+			.attr('src', customURL)
+			.width(16)
+			.height(16);
+	});
+	$('.custom').click(function(){
+		if( $('.customURL').val().length === 0 ) {
+		    $('.favicon').attr('src', 'assets/images/google-favicon.svg');
+		}
+	});
+	$(".customURL").keydown(function(e){
+	    if(e.which === 13){
+	        $(".custom").click();
+	    }
+	});
+	$('.about').on('click', function() {
+		$('.overlay').show();
+		$('.overlay-bg').addClass('in');
+		$('.overlay-container').addClass('in');
+	});
+	$('.overlay-bg, .close-overlay').on('click', function() {
+		$('.overlay').hide();
+		$('.overlay-bg').removeClass('in');
+		$('.overlay-container').removeClass('in');
+	})
+})
